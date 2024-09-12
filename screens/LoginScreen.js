ee5e76aceacce756b20da19d3d59d6304246e290
Crisvargasmz <cristhiancesarvargas12@gmail.com>
2024-09-12 10:06:08 -0600

    Subiendo a rama crishtian

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,6 +6,7 @@ import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 import { db } from '../config/firebaseConfig';
 import Icon from 'react-native-vector-icons/FontAwesome';
+//probando rama
 
 // URL del fondo del login
 const url = 'https://ak.picdn.net/shutterstock/videos/1060308725/thumb/1.jpg';
