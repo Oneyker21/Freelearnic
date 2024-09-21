6a67b744d6252320bf4779fc9dd91ecde5805c95
Crisvargasmz <cristhiancesarvargas12@gmail.com>
2024-09-21 16:41:31 -0600

    Fuentes de letras y componente que las llama.

--- App.js
+++ App.js
@@ -1,17 +1,45 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "./screens/LoginScreen";
-import HomeScreen from "./screens/freelancer/HomeScreen";
-import RegisterUsers from "./screens/RegisterUsers";
-import HomeScreenSb from "./screens/cliente/HomeScreenSb";
-import HomeScreenFreelancer from "./screens/freelancer/HomeScreenFreelancer";
-import RegisterFreelancer from "./screens/freelancer/RegisterFreelancer";
-import CreateProject from "./screens/cliente/CreateProject";
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ActivityIndicator, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans'; // Importar Open Sans
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/freelancer/HomeScreen';
+import RegisterUsers from './screens/RegisterUsers';
+import HomeScreenSb from './screens/cliente/HomeScreenSb';
+import HomeScreenFreelancer from './screens/freelancer/HomeScreenFreelancer';
+import RegisterFreelancer from './screens/freelancer/RegisterFreelancer';
+import CreateProject from './screens/cliente/CreateProject';
 
 const Stack = createNativeStackNavigator();
 
+const loadFonts = async () => {
+  await Font.loadAsync({
+    'Roboto-regular': Roboto_400Regular,
+    'Roboto-bold': Roboto_700Bold,
+    'OpenSans-regular': OpenSans_400Regular, // Cargar Open Sans Regular
+    'OpenSans-bold': OpenSans_700Bold,       // Cargar Open Sans Bold
+  });
+};
+
 const App = () => {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadResources = async () => {
+      await loadFonts();
+      setFontsLoaded(true);
+    };
+
+    loadResources();
+  }, []);
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
