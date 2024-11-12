18dd8f6b687d3c740f25a96b14410662c8524850
Oneyker21 <oneygaleano21@gmail.com>
2024-11-12 11:06:42 -0600

    barra search freelancer

--- screens/client/SearchFreelancers.js
+++ screens/client/SearchFreelancers.js
@@ -1,161 +1,192 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, FlatList, ActivityIndicator, TouchableOpacity, TextInput } from 'react-native';
+import { View, Text, StyleSheet, FlatList, ActivityIndicator, TouchableOpacity, TextInput, Dimensions } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { db } from '../../connection/firebaseConfig';
 import { collection, onSnapshot } from 'firebase/firestore';
 import { CustomPicker } from '../../utils/inputs';
 
+const { width, height } = Dimensions.get("window");
+
 const SearchFreelancers = () => {
-    const [freelancers, setFreelancers] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [professionFilter, setProfessionFilter] = useState('');
-    const [filteredFreelancers, setFilteredFreelancers] = useState([]);
-    const [searchQuery, setSearchQuery] = useState('');
-  
-    // Opciones de profesiones para el filtro
-    const professions = [
-      { label: 'Todos', value: '' },
-      { label: 'Ingeniera en sistemas', value: 'Ingeniera en sistemas' },
-      { label: 'Programador', value: 'Programador' },
-      { label: 'Diseñador gráfico', value: 'Diseñador gráfico' },
-      // Agrega otras profesiones según tu base de datos
-    ];
-  
-    useEffect(() => {
-      const unsubscribe = onSnapshot(collection(db, 'Freelancers'), (querySnapshot) => {
-        const freelancersData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-        setFreelancers(freelancersData);
-        filterFreelancers(searchQuery, professionFilter, freelancersData);
-        setLoading(false);
-      }, (error) => {
-        console.error('Error fetching freelancers: ', error);
-        setLoading(false);
-      });
-  
-      return () => unsubscribe();
-    }, [searchQuery, professionFilter]);
-  
-    // Filtrar freelancers por búsqueda y profesión
-    const filterFreelancers = (text, profession, freelancersData) => {
-      let filtered = freelancersData;
-  
-      // Filtrar por profesión
-      if (profession !== '') {
-        filtered = filtered.filter(freelancer => freelancer.profession === profession);
-      }
-  
-      // Filtrar por nombre de usuario
-      if (text !== '') {
-        filtered = filtered.filter(freelancer => freelancer.username.toLowerCase().includes(text.toLowerCase()));
-      }
-  
-      setFilteredFreelancers(filtered);
-    };
-  
-    // Filtrar freelancers por nombre de usuario en tiempo real
-    const handleSearch = (text) => {
-      setSearchQuery(text);
-      if (text === '') {
-        setFilteredFreelancers(freelancers);
-      } else {
-        const filtered = freelancers.filter(freelancer => freelancer.username.toLowerCase().includes(text.toLowerCase()));
-        setFilteredFreelancers(filtered);
-      }
-    };
-  
-    if (loading) {
-      return <ActivityIndicator size="large" color="#007AFF" />;
+  const [freelancers, setFreelancers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [professionFilter, setProfessionFilter] = useState('');
+  const [filteredFreelancers, setFilteredFreelancers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  // Opciones de profesiones para el filtro
+  const professions = [
+    { label: 'Todos', value: '' },
+    { label: 'Ingeniera en sistemas', value: 'Ingeniera en sistemas' },
+    { label: 'Programador', value: 'Programador' },
+    { label: 'Diseñador gráfico', value: 'Diseñador gráfico' },
+    // Agrega otras profesiones según tu base de datos
+  ];
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, 'Freelancers'), (querySnapshot) => {
+      const freelancersData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setFreelancers(freelancersData);
+      filterFreelancers(searchQuery, professionFilter, freelancersData);
+      setLoading(false);
+    }, (error) => {
+      console.error('Error fetching freelancers: ', error);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, [searchQuery, professionFilter]);
+
+  // Filtrar freelancers por búsqueda y profesión
+  const filterFreelancers = (text, profession, freelancersData) => {
+    let filtered = freelancersData;
+
+    // Filtrar por profesión
+    if (profession !== '') {
+      filtered = filtered.filter(freelancer => freelancer.profession === profession);
     }
-  
-    return (
-        <View style={styles.container}>
-          {/* Barra de búsqueda */}
-          <TextInput
-            style={styles.searchBar}
-            placeholder="Buscar por nombre de usuario"
-            value={searchQuery}
-            onChangeText={handleSearch}
-          />
-      
-          {/* Filtro por profesión */}
-          <CustomPicker
-            style={styles.selectType}
-            selectedValue={professionFilter}
-            onValueChange={setProfessionFilter}
-            items={professions}
-            placeholder="Seleccionar profesión"
-          />
-      
-          <FlatList
-            contentContainerStyle={styles.listContainer} // Estilo adicional para el contenido de la lista
-            data={filteredFreelancers}
-            keyExtractor={item => item.id}
-            renderItem={({ item }) => (
-              <View style={styles.card}>
-                <Text style={styles.freelancerName}>{item.firstName} {item.lastName}</Text>
-                <Text style={styles.freelancerUsername}>Usuario: {item.username}</Text>
-                <Text style={styles.freelancerProfession}>Profesión: {item.profession}</Text>
-                <Text style={styles.freelancerLocation}>Ciudad: {item.city}, {item.state}</Text>
-                <Text style={styles.freelancerVerified}>Verificado: {item.verified ? 'Sí' : 'No'}</Text>
-              </View>
-            )}
-          />
-        </View>
-      );      
+
+    // Filtrar por nombre de usuario
+    if (text !== '') {
+      filtered = filtered.filter(freelancer => freelancer.username.toLowerCase().includes(text.toLowerCase()));
+    }
+
+    setFilteredFreelancers(filtered);
   };
-  
-  const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16, // Margen alrededor del contenedor principal
-        backgroundColor: '#f4f4f4',
-        marginTop: 30,
-      },
-    searchBar: {
-      height: 40,
-      borderColor: '#ccc',
-      borderWidth: 1,
-      borderRadius: 8,
-      paddingLeft: 10,
-      margin: 10,
+
+  // Filtrar freelancers por nombre de usuario en tiempo real
+  const handleSearch = (text) => {
+    setSearchQuery(text);
+    if (text === '') {
+      setFilteredFreelancers(freelancers);
+    } else {
+      const filtered = freelancers.filter(freelancer => freelancer.username.toLowerCase().includes(text.toLowerCase()));
+      setFilteredFreelancers(filtered);
+    }
+  };
+
+  // Función para manejar el evento de presión del botón de búsqueda
+  const handleSearchPress = () => {
+    filterFreelancers(searchQuery, professionFilter, freelancers);
+  };
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="#007AFF" />;
+  }
+
+  return (
+    <View style={styles.container}>
+      {/* Barra de búsqueda */}
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchBar}
+          placeholder="Buscar"
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+        />
+        <TouchableOpacity
+          style={styles.searchButton}
+          onPress={handleSearchPress}
+        >
+          <Ionicons name="search" size={26} color="white" />
+        </TouchableOpacity>
+      </View>
+
+      {/* Filtro por profesión */}
+      <CustomPicker
+        style={styles.selectType}
+        selectedValue={professionFilter}
+        onValueChange={setProfessionFilter}
+        items={professions}
+        placeholder="Seleccionar profesión"
+      />
+
+      <FlatList
+        contentContainerStyle={styles.listContainer} // Estilo adicional para el contenido de la lista
+        data={filteredFreelancers}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Text style={styles.freelancerName}>{item.firstName} {item.lastName}</Text>
+            <Text style={styles.freelancerUsername}>Usuario: {item.username}</Text>
+            <Text style={styles.freelancerProfession}>Profesión: {item.profession}</Text>
+            <Text style={styles.freelancerLocation}>Ciudad: {item.city}, {item.state}</Text>
+            <Text style={styles.freelancerVerified}>Verificado: {item.verified ? 'Sí' : 'No'}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16, // Margen alrededor del contenedor principal
+    backgroundColor: '#f4f4f4',
+    marginTop: 30,
+  },
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    margin: width * 0.025,
+  },
+  searchBar: {
+    flex: 1,
+    height: height * 0.05,
+    borderColor: "#ccc",
+    backgroundColor: "#D9D9D9",
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingLeft: width * 0.025,
+    marginLeft: width * 0.01,
+  },
+  searchButton: {
+    height: height * 0.049,
+    backgroundColor: "#007AFF",
+    paddingVertical: height * 0.009,
+    paddingHorizontal: width * 0.03,
+    borderRadius: 8,
+    marginLeft: width * 0.02,
+  },
+  selectType: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingLeft: 10,
+    margin: 10,
+  },
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    padding: 15,
+    marginVertical: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    selectType: {
-      height: 40,
-      borderColor: '#ccc',
-      borderWidth: 1,
-      borderRadius: 8,
-      paddingLeft: 10,
-      margin: 10,
-    },
-    card: {
-      backgroundColor: '#fff',
-      borderRadius: 8,
-      padding: 15,
-      marginVertical: 10,
-      shadowColor: '#000',
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.2,
-      shadowRadius: 2.62,
-      elevation: 4,
-    },
-    freelancerName: {
-      fontWeight: 'bold',
-      fontSize: 16,
-    },
-    freelancerUsername: {
-      color: '#666',
-    },
-    freelancerProfession: {
-      color: '#666',
-    },
-    freelancerLocation: {
-      color: '#666',
-    },
-    freelancerVerified: {
-      color: '#666',
-    },
-  });
+    shadowOpacity: 0.2,
+    shadowRadius: 2.62,
+    elevation: 4,
+  },
+  freelancerName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  freelancerUsername: {
+    color: '#666',
+  },
+  freelancerProfession: {
+    color: '#666',
+  },
+  freelancerLocation: {
+    color: '#666',
+  },
+  freelancerVerified: {
+    color: '#666',
+  },
+});
 
 export default SearchFreelancers;
